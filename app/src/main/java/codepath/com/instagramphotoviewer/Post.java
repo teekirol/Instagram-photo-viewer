21d3fa8f24b56588338104bd7cffc555eba4750d
Lori Lee <leelorik@gmail.com>
2015-02-05 09:04:45 -0800

    Create a Post out of a JSON object

--- app/src/main/java/codepath/com/instagramphotoviewer/Post.java
+++ app/src/main/java/codepath/com/instagramphotoviewer/Post.java
@@ -1,5 +1,12 @@
 package codepath.com.instagramphotoviewer;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.jar.JarException;
+
 public class Post {
 
     public String username;
@@ -8,6 +15,41 @@ public class Post {
     public String caption;
     public String photoUrl;
     public int numLikes;
+
+    public Post(JSONObject object) {
+        try {
+
+            JSONObject user = object.getJSONObject("user");
+            this.username = user.getString("username");
+            this.avatarUrl = user.getString("profile_picture");
+
+            JSONObject caption = object.getJSONObject("caption");
+            this.createdTime = caption.getString("created_time");
+            this.caption = caption.getString("text");
+
+            JSONObject images = object.getJSONObject("images");
+            this.photoUrl = images.getJSONObject("standard_resolution").getString("url");
+
+            JSONObject likes = object.getJSONObject("likes");
+            this.numLikes = likes.getInt("count");
+
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Post> fromJson(JSONArray array) {
+        ArrayList<Post> posts = new ArrayList<Post>(array.length());
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                posts.add(new Post(array.getJSONObject(i)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return posts;
+    }
 
     public String getUsername() {
         return username;
